8e330a35bf197ab0fafeb8d9396da84ef28496f5
Belin27000 <y.lecerf@protonmail.com>
2024-12-01 18:34:00 +0100

    [FEAT] SunFi addClient function on Smart contract and test on client

--- backend/contracts/SunFi.sol
+++ backend/contracts/SunFi.sol
@@ -6,5 +6,32 @@ pragma solidity ^0.8.27;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SunFi is Ownable {
+    struct Client {
+        bool isRegistered;
+    }
+
+    mapping(address => Client) clients;
+
+    event ClientRegistered(address clientAdress);
+
     constructor() payable Ownable(msg.sender) {}
+
+    // ::::::::::::: GETTERS ::::::::::::: //
+
+    function getClient(address _addr) external view returns (Client memory) {
+        return clients[_addr];
+    }
+
+    // ::::::::::::: CLIENT REGISTRATION ::::::::::::: //
+
+    function addClient(address _addr) external onlyOwner {
+        require(_addr != owner(), "Owner cannot be registered as a client");
+        require(
+            clients[_addr].isRegistered != true,
+            "This adress already registered as a client address!"
+        );
+        clients[_addr].isRegistered = true;
+
+        emit ClientRegistered(_addr);
+    }
 }
